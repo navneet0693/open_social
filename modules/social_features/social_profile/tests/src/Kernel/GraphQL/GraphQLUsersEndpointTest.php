f4d81fbc213170818117b18a1d346d9097827b81
Alexander Varwijk <alexander@getopensocial.com>
2021-07-12 10:40:04 +0200

    Refactor profile list and field access checks
    This moves the access check for `mail` and `langcode` which we deemed
    priviliged into field access checks which allows other Drupal systems to
    more easily check them.
    
    Additionally this introduces a new `list user` (in the form `list
    <entity>`) permission ahead of
    https://www.drupal.org/project/drupal/issues/2953566 that is required
    for users to create a listing of users.
    
    The permission to view users and profiles is needed for every user of
    Open Social since we otherwise can't render things like actvitities for
    anonymous or authenticated users. However, we have custmo access checks
    on overview pages to prevent anonymous users from easily listing all
    users on a platform. Such access check was not yet available as
    permission which meant that the GraphQL API could inadvertently list all
    users.

--- modules/social_features/social_profile/tests/src/Kernel/GraphQL/GraphQLUsersEndpointTest.php
+++ modules/social_features/social_profile/tests/src/Kernel/GraphQL/GraphQLUsersEndpointTest.php
@@ -1,11 +1,12 @@
 <?php
 
-namespace Drupal\Tests\social_profile\Kernel;
+namespace Drupal\Tests\social_profile\Kernel\GraphQL;
 
 use Drupal\Core\Session\AccountInterface;
 use Drupal\profile\Entity\ProfileInterface;
 use Drupal\Tests\social_graphql\Kernel\SocialGraphQLTestBase;
 use Drupal\user\Entity\User;
+use Drupal\user\UserInterface;
 
 /**
  * Tests the additions made to the user endpoint by this module.
@@ -84,7 +85,9 @@ class GraphQLUsersEndpointTest extends SocialGraphQLTestBase {
     // Create a set of 10 test users that we can query. The data of the users
     // shouldn't matter.
     for ($i = 0; $i < 10; ++$i) {
-      $this->users[] = $this->createUser();
+      $user = $this->createUser();
+      self::assertInstanceOf(UserInterface::class, $user, "Test set-up failed: could not create user.");
+      $this->users[] = $user;
     }
   }
 
@@ -101,52 +104,128 @@ class GraphQLUsersEndpointTest extends SocialGraphQLTestBase {
     // available in an all-access scenario.
     $this->setUpCurrentUser([], [], TRUE);
     $test_user = $this->createUser();
+    self::assertInstanceOf(UserInterface::class, $test_user, "Test set-up failed: could not create user.");
     $profile = $this->ensureTestProfile($test_user, 'profile');
-    $query = "
-      query {
-        user(id: \"{$test_user->uuid()}\") {
-          profile {
-            firstName
-            lastName
-            introduction {
-              format {
-                name
+
+    $first_name = $profile->get('field_profile_first_name')->first();
+    $last_name = $profile->get('field_profile_last_name')->first();
+    /** @var \Drupal\Core\Field\FieldItemInterface|NULL $self_introduction */
+    $self_introduction = $profile->get('field_profile_self_introduction')->first();
+    $phone_number = $profile->get('field_profile_phone_number')->first();
+    $function = $profile->get('field_profile_function')->first();
+    $organization = $profile->get('field_profile_organization')->first();
+
+    self::assertNotNull($first_name);
+    self::assertNotNull($last_name);
+    self::assertNotNull($self_introduction);
+    self::assertNotNull($phone_number);
+    self::assertNotNull($function);
+    self::assertNotNull($organization);
+
+    $self_introduction_format = $self_introduction->get('format');
+    $self_introduction_value = $self_introduction->get('value');
+    $self_introduction_processed = $self_introduction->get('processed');
+
+    self::assertNotNull($self_introduction_format);
+    self::assertNotNull($self_introduction_value);
+    self::assertNotNull($self_introduction_processed);
+
+    $this->assertResults(
+      '
+        query ($id: ID!) {
+          user(id: $id) {
+            profile {
+              firstName
+              lastName
+              introduction {
+                format {
+                  name
+                }
+                raw
+                processed
               }
-              raw
-              processed
+              phone
+              function
+              organization
             }
-            phone
-            function
-            organization
           }
         }
-      }
-    ";
-    $expected_data = [
-      'data' => [
+      ',
+      ['id' => $test_user->uuid()],
+      [
         'user' => [
           'profile' => [
-            'firstName' => $profile->get('field_profile_first_name')->first()->getString(),
-            'lastName' => $profile->get('field_profile_last_name')->first()->getString(),
+            'firstName' => $first_name->getString(),
+            'lastName' => $last_name->getString(),
             'introduction' => [
               'format' => [
-                'name' => $profile->get('field_profile_self_introduction')->first()->get('format')->getString(),
+                'name' => $self_introduction_format->getString(),
               ],
-              'raw' => $profile->get('field_profile_self_introduction')->first()->get('value')->getString(),
-              'processed' => $profile->get('field_profile_self_introduction')->first()->get('processed')->getString(),
+              'raw' => $self_introduction_value->getString(),
+              'processed' => $self_introduction_processed->getString(),
             ],
-            'phone' => $profile->get('field_profile_phone_number')->first()->getString(),
-            'function' => $profile->get('field_profile_function')->first()->getString(),
-            'organization' => $profile->get('field_profile_organization')->first()->getString(),
+            'phone' => $phone_number->getString(),
+            'function' => $function->getString(),
+            'organization' => $organization->getString(),
           ],
         ],
       ],
-    ];
+      $this->defaultCacheMetaData()
+        ->addCacheableDependency($test_user)
+        ->addCacheableDependency($profile)
+        ->addCacheContexts(['languages:language_interface'])
+        ->addCacheTags(['profile_list'])
+    );
+  }
 
-    // @todo Move to QueryResultAssertionTrait::assertResults and add metadata.
-    $result = $this->query($query);
-    self::assertSame(200, $result->getStatusCode(), 'user fields are present');
-    self::assertSame($expected_data, json_decode($result->getContent(), TRUE), 'user fields are present');
+  /**
+   * Test that additional permissions are required for profile fields.
+   */
+  public function testRequiresPermissionForProfileFields() : void {
+    $this->setUpCurrentUser([], ['access user profiles']);
+    $test_user = $this->createUser();
+    self::assertInstanceOf(UserInterface::class, $test_user, "Test set-up failed: could not create user.");
+    $this->ensureTestProfile($test_user, 'profile');
+
+    $this->assertResults(
+      '
+        query ($id: ID!) {
+          user(id: $id) {
+            profile {
+              firstName
+              lastName
+              introduction {
+                format {
+                  name
+                }
+                raw
+                processed
+              }
+              phone
+              function
+              organization
+            }
+          }
+        }
+      ',
+      ['id' => $test_user->uuid()],
+      [
+        'user' => [
+          'profile' => [
+            'firstName' => NULL,
+            'lastName' => NULL,
+            'introduction' => NULL,
+            'phone' => NULL,
+            'function' => NULL,
+            'organization' => NULL,
+          ],
+        ],
+      ],
+      $this->defaultCacheMetaData()
+        ->addCacheableDependency($test_user)
+        ->addCacheContexts(['languages:language_interface'])
+        ->addCacheTags(['profile_list'])
+    );
   }
 
   /**
@@ -164,6 +243,7 @@ class GraphQLUsersEndpointTest extends SocialGraphQLTestBase {
     /** @var \Drupal\profile\ProfileStorageInterface $profile_storage */
     $profile_storage = $this->container->get('entity_type.manager')->getStorage('profile');
     $profile = $profile_storage->loadByUser($user, $profile_type);
+    self::assertInstanceOf(ProfileInterface::class, $profile, "Test set-up failed: could not load profile.");
     $profile
       ->set('field_profile_first_name', $this->randomString())
       ->set('field_profile_last_name', $this->randomString())
