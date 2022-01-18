faba0dd029edc6bbb109cb4a61048efe6c549985
serhii.myronets <serhii.myronets@lemberg.co.uk>
2022-01-18 17:40:17 +0200

    Issue #3179171 by agami4: Remove js library

--- modules/social_features/social_profile/assets/js/admin.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  Drupal.behaviors.socialProfileAdmin = {
-    attach: function (context) {
-      // Rewrite value of the autocomplete attribute which get from jquery.ui js file.
-      $('.field--name-field-profile-org-details input[id*="edit-field-profile-org-details"]',context)
-        .once('socialProfileAdmin').attr('autocomplete', 'organization-title');
-    }
-  };
-})(jQuery);
